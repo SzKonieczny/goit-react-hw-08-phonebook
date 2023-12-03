e3651d6f078588036ed33c74d3ae881e08942f4c
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-12-04 00:22:42 +0100

    App added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { PhonebookForm } from './PhonebookFrom/PhonebookFrom';
+
+import {
+  MainContainer,
+  PhonebookContainer,
+  Title,
+  TitleCont,
+} from './app.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <MainContainer>
+      <Title>PhoneBook</Title>
+
+      <PhonebookContainer>
+        <PhonebookForm />
+      </PhonebookContainer>
+      <TitleCont>Contacts</TitleCont>
+      <Filter />
+      <ContactList />
+      <Toaster />
+    </MainContainer>
   );
 };
