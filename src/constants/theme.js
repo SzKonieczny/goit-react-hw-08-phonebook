eddefde555612e1c9981579df5baeae4bda5604b
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-12-03 23:01:19 +0100

    package.json; constants; redux added

---
+++ src/constants/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  gradients: {},
+  filters: {
+    shadow: '0px 0.7px 3px rgba(0, 0, 0, 0.32)',
+  },
+};
---
+++ src/redux/spliceContacts.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const contactsApi = createApi({
+  reducerPath: 'contactsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://656277f1ee04015769a670ac.mockapi.io/mockapi/v1/',
+  }),
+  tagTypes: ['Contacts'],
+  endpoints: builder => ({
+    getContacts: builder.query({
+      query: () => `contacts`,
+      providesTags: ['Contacts'],
+    }),
+    createContact: builder.mutation({
+      query: newContact => ({
+        url: 'contacts',
+        method: 'POST',
+        body: newContact,
+      }),
+
+      invalidatesTags: ['Contacts'],
+    }),
+    deleteContact: builder.mutation({
+      query: contactId => ({
+        url: `contacts/${contactId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Contacts'],
+    }),
+  }),
+});
+export const {
+  useGetContactsQuery,
+  useDeleteContactMutation,
+  useCreateContactMutation,
+} = contactsApi;
